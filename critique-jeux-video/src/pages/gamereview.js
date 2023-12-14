69fc645206205b15a40a4d4324fd14776f420347
William <william.snoeck@std.heh.be>
2023-12-14 11:38:48 +0100

    first commit

--- critique-jeux-video/src/pages/gamereview.js
+++ critique-jeux-video/src/pages/gamereview.js
@@ -1,12 +1,11 @@
-// gamereview.js
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import ListGame from '../components/listGame'; // Importez le composant ListGame
-import stylesDetail from '../styles/details.module.css'; 
+import ListGame from '../components/listGame';
+import stylesDetail from '../styles/details.module.css';
 
 const GameReview = () => {
   const { id } = useParams();
-  const gameList = ListGame.props.gameList; // Accédez à la propriété gameList du composant ListGame
+  const gameList = ListGame.props.gameList;
   const game = gameList.find((g) => g.id.toString() === id);
 
   if (!game) {
@@ -14,21 +13,29 @@ const GameReview = () => {
   }
 
   return (
-    <div style={{ display: 'flex', textAlign:'center' }} className={stylesDetail['background']}>
-      <div style={{ flex: 3, padding: '20px' }}>
-        {/* Côté gauche (75%) */}
-        <h1 style={{ color: '#fff', textTransform: 'uppercase' }}>{game.name}</h1>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh', background: '#282c34', color: '#fff', padding: '20px' }}>
+      <div style={{ maxWidth: '800px', width: '100%' }}>
+        <h1 style={{ textTransform: 'uppercase', fontSize: '2em', marginBottom: '20px' }}>{game.name}</h1>
 
-        <img src={game.image} alt={game.name.toUpperCase()} style={{ width: '500px', height: '500px' }} />
-        <h2 style={{ color: '#fff' }}>Description du jeu : </h2>
-        <p style={{ color: '#fff', fontSize: '21px' }}>{game.description}</p>
+        <div style={{ marginBottom: '20px' }}>
+          <img src={game.image} alt={game.name.toUpperCase()} style={{ width: '100%', borderRadius: '8px', boxShadow: '0 0 10px rgba(0,0,0,0.3)' }} />
+        </div>
+
+        <h2 style={{ fontSize: '1.5em', marginBottom: '10px' }}>Description du jeu :</h2>
+        <p style={{ fontSize: '1.2em' }}>{game.description}</p>
+
         {/* Ajoutez des détails supplémentaires au besoin */}
-      </div>
-      <div style={{ flex: 1, padding: '20px'} }>
-        {/* Liste des commentaires */}
-        <div style={{ position: 'fixed', bottom: 25 }}>
-          <input type="text" placeholder="Ajouter un commentaire" required style={{ width:'300px' }} />
-          <input type="submit"  value="send" style={{ marginLeft: 3 }}/>
+        
+        <div style={{ marginTop: '40px', borderTop: '1px solid #fff', paddingTop: '20px' }}>
+          {/* Liste des commentaires */}
+          <h3 style={{ marginBottom: '20px', fontSize: '1.5em' }}>Commentaires</h3>
+          {/* Ajoutez la liste des commentaires ici si disponible */}
+
+          {/* Formulaire de commentaire */}
+          <div style={{ display: 'flex', marginBottom: '20px' }}>
+            <input type="text" placeholder="Ajouter un commentaire" required style={{ flex: 1, padding: '8px', marginRight: '10px', fontSize: '1em' }} />
+            <button style={{ background: '#61dafb', color: '#fff', border: 'none', padding: '8px 15px', cursor: 'pointer', fontSize: '1em' }}>Envoyer</button>
+          </div>
         </div>
       </div>
     </div>
